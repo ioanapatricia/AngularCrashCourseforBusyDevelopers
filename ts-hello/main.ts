2b36543af41c66da91199553bbc56c68ecf560fc
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-03-27 14:03:52 +0200

    2.15 Types
    -data types in typescript

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,18 +1,13 @@
-function doSomething() {
-    for (var i = 0; i<5; i++) {
-        console.log(i);
-    }
+ let a: number;
+ let b: boolean;
+ let c: string;
+ let d: any;
+ let e: number[] = [1,2,3];
+ let f: any[] = [1, true, 'a', false];
 
-    console.log('Finally: ' + i);
-}
+ const ColorRed = 0;
+ const ColorGreen = 1;
+ const ColorBlue = 2;
 
-function doSomethingElse() {
-    for (let i = 0; i<5; i++) {
-        console.log(i);
-    }
-
-    console.log('Finally: ' + i);
-}
-
-doSomething();
-doSomethingElse()
+ enum Color {Red = 0, Green = 1, Blue = 2, Purple = 3};
+ let backgroundColor = Color.Red;
