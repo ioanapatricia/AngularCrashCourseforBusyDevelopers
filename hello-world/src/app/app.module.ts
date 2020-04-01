aadff6732726ba0d984433bfdaba8516fa639621
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-04-01 13:12:00 +0300

    3.31 Generating Components Using Angular CLI

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 import { CoursesComponent } from './courses.component';
 
 
 @NgModule({
    declarations: [
       AppComponent,
+      CourseComponent,
       CoursesComponent
    ],
    imports: [
