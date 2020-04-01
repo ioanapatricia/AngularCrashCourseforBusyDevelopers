a79f1fc882e371d18ddc32515a3de64932d00d0d
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-04-01 11:58:01 +0300

    2.25 Modules
    -modules --> in ts each file can be a module.
    -to make the Point class accessible ( to use it in other parts of the program) --> added the export keyword.
    -now that I'm exporting something, from ts point of view the point.ts file is a module.
    -in main.ts I imported the class to be able to use it.

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,25 +1,4 @@
-class Point {
-    constructor(private _x?: number, private _y?: number) {
-    }
-
-    draw() {
-        console.log('X: ' + this._x + ',Y: ' + this._y);
-    }
-
-    get x() {
-        return this._x;
-    }
-
-    set x(value) {
-        if (value < 0)
-            throw new Error('value cannot be less than zero');
-        
-        this._x = value;  
-            
-    }
-}
+import { Point } from './point';
 
 let point = new Point(1, 2);
-let x = point.x;
-point.x = 10;
 point.draw();
