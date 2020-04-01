1f41c11ddba8e04686d0aab261bab7b314415f28
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-04-01 13:06:30 +0300

    3.30 Components

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      CoursesComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'app-courses',
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+    
+}
