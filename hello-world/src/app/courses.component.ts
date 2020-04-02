66d0e3c093251d8e2058c6afc20444fe7d7a130a
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-04-02 13:19:46 +0300

    4.40 Adding Bootstrap

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,12 +4,7 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'app-courses',
     template: `
-        <h2>{{ title }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{ course }}
-            </li>
-        </ul>
+            <button class="btn btn-primary">Save</button>
         `
 })
 export class CoursesComponent {
