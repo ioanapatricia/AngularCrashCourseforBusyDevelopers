a4f3deac604a3f7326e16b3bceadfdb2f1c3a6c0
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-04-10 11:57:19 +0300

    9.108 OnInit Interface
    -ngOnInit --> is the method Angular calls when it intializes my component.
    - the http services must not be called in the component's constructor. Needing initialisation ? use ngOnInit.

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -6,38 +6,15 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent  {
+export class PostsComponent implements OnInit{
   posts: any[];
   private url = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http: HttpClient) {
-    http.get(this.url).subscribe(response => {
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit() {
+    this.http.get(this.url).subscribe(response => {
       this.posts = response as any[];
-    });
-  }
-
-  createPost(input: HTMLInputElement) {
-    const post: any = { title: input.value };
-    input.value = '';
-
-    this.http.post(this.url, JSON.stringify(post)).subscribe(response => {
-      post.id = response.id;
-      this.posts.splice(0, 0, post);
-      console.log(response);
-    });
-  }
-
-  updatePost(post) {
-    // this.http.put(this.url, JSON.stringify(post));
-    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true})).subscribe(response => {
-      console.log(response);
-    });
-  }
-
-  deletePost(post) {
-    this.http.delete(this.url + '/' + post.id).subscribe(response => {
-      let index =  this.posts.indexOf(post);
-      this.posts.splice(index, 1);
     });
   }
 }
