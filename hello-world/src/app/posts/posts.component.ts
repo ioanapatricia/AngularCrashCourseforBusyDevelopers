80fc8a45519273b74c1fa33fe35f45a1f4fa0a9c
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-04-10 11:50:35 +0300

    9.107 Deleting Data
    -by convention Http Delete req don't have a body.
    -all I need to send to the server is a http delete req to this endpoint and then subscribe.
    -to delete the post from the array I need to find the index and use the splice method.

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -33,4 +33,11 @@ export class PostsComponent  {
       console.log(response);
     });
   }
+
+  deletePost(post) {
+    this.http.delete(this.url + '/' + post.id).subscribe(response => {
+      let index =  this.posts.indexOf(post);
+      this.posts.splice(index, 1);
+    });
+  }
 }
