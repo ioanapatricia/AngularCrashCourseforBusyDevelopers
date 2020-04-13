575c91e0b4f432847b7934e051677ad6da8b29ab
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-04-13 13:41:13 +0300

    9.122 Observable vs Promises
    -observables can be converted to promises.
    -prefer observables to promises.
    -obs allow Reactive Programming.
    -provide a bunch of useful operators.
    -they are lazy => nothing happens until you subscribe.

--- hello-world/src/app/services/data.service.ts
+++ hello-world/src/app/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
-import { Observable, throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { AppError } from '../common/app-error';
 import { NotFoundError } from '../common/not-found-error';
 import { BadInput } from '../common/bad-input';
