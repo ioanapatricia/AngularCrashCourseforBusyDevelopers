75475276a9afbb6f1587f486fcb2eaef8f435e9f
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-04-15 15:38:01 +0300

    10.129 Getting the Route Parameters

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -67,7 +67,7 @@ import { RouterModule } from '@angular/router';
       HttpClientModule,
       RouterModule.forRoot([
          { path: '', component: HomeComponent },
-         { path: 'followers/:username', component: GithubProfileComponent },
+         { path: 'followers/:id', component: GithubProfileComponent },
          { path: 'followers', component: GithubFollowersComponent },
          { path: 'posts', component: PostsComponent },
          { path: '**', component: NotfoundComponent }
