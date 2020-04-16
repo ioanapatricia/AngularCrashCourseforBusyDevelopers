d667fe320c17d0cb59ba48ca0ffb641b0b156923
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-04-16 12:53:38 +0300

    10.135 Programmatic Navigation

--- hello-world/src/app/github-profile/github-profile.component.ts
+++ hello-world/src/app/github-profile/github-profile.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css']
 })
-export class GithubProfileComponent implements OnInit {
+export class GithubProfileComponent  {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    console.log(id);
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest'}
+    });
   }
 
 }
